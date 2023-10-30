9d5aab47972bdf2d48358482a56ddbd120c3c75e
Jakov Milic <jmilic@protonmail.com>
2023-10-30 13:04:12 +0100

    Fix treesitter config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,46 +3,48 @@ return {
     dependencies = {
         "nvim-treesitter/nvim-treesitter-textobjects",
     },
-    opts = {
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "javascript", "typescript" },
-        sync_install = false,
-        auto_install = true,
-        highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = false,
-        },
-        textobjects = {
-            select = {
+    config = function ()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "javascript", "typescript" },
+            sync_install = false,
+            auto_install = true,
+            highlight = {
                 enable = true,
-                lookahead = true,
-                keymaps = {
-                    ["af"] = "@function.outer",
-                    ["if"] = "@function.inner",
-                    ["ac"] = "@class.outer",
-                    ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-                    ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+                additional_vim_regex_highlighting = false,
+            },
+            textobjects = {
+                select = {
+                    enable = true,
+                    lookahead = true,
+                    keymaps = {
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                        ["ac"] = "@class.outer",
+                        ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+                        ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+                    },
+                },
+                move = {
+                    enable = true,
+                    lookahead = true,
+                    goto_next_start = {
+                        ["mf"] = "@function.outer",
+                        ["mc"] = "@class.outer",
+                    },
+                    goto_next_end = {
+                        ["mef"] = "@function.outer",
+                        ["mec"] = "@class.outer",
+                    },
+                    goto_previous_start = {
+                        ["Mf"] = "@function.outer",
+                        ["Mc"] = "@class.outer",
+                    },
+                    goto_previous_end = {
+                        ["mef"] = "@function.outer",
+                        ["mec"] = "@class.outer",
+                    },
                 },
             },
-            move = {
-                enable = true,
-                lookahead = true,
-                goto_next_start = {
-                    ["mf"] = "@function.outer",
-                    ["mc"] = "@class.outer",
-                },
-                goto_next_end = {
-                    ["mef"] = "@function.outer",
-                    ["mec"] = "@class.outer",
-                },
-                goto_previous_start = {
-                    ["Mf"] = "@function.outer",
-                    ["Mc"] = "@class.outer",
-                },
-                goto_previous_end = {
-                    ["mef"] = "@function.outer",
-                    ["mec"] = "@class.outer",
-                },
-            },
-        },
-    },
+        })
+    end,
 }
